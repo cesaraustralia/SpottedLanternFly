87e0385507e735cbd2f3c96a68510b12ea149736
jamesmaino <jamesmaino@gmail.com>
2020-12-10 12:53:15 +1100

    nearly working

--- establishment.jl
+++ establishment.jl
@@ -4,54 +4,82 @@
 using Interpolations, TimeInterpolatedGeoData, RasterDataSources, Test, Dates, GeoData
 using RasterDataSources: Values, SoilMoisture, Upper, Lower
 
-# Weather time-series
-dates = (Date(2001), Date(2002))
-layers = (:tmin, :tmax)
-ser = series(WorldClim{Weather}; layers=layers, dates=dates, window=(Lat(600:1900), Lon(600:1900)))
-ser[Date(2001, 1)][:tmax]
-# Select Australia, using regular lat/lon selectors
-A = geoarray(WorldClim{Weather}, :prec, Date(2001, 05); mappedcrs=EPSG(4326))
-A[Lat(Between(-10, -45)), Lon(Between(110, 160))]
+############### new test
 
-# Weather time-series
+layers = (:tmin, :tmax)
+months = 1:12
+download_raster(WorldClim{Climate}, layers; month=months)
+
+ser = series(WorldClim{Climate}; layers=layers)
+ser = set(ser, Ti=(DateTime(2001, 1, 1):Month(1):DateTime(2001, 12, 1)))
+# We use a DimensionalData dim instead of a vector of dates because a
+# Dim can have a step size with irregular spaced data - here 1 Hour.
+dates = Ti(vec([d + h for h in Hour.(0:23), d in index(ser, Ti)]);
+    mode=Sampled(Ordered(), Regular(Hour(1)), Intervals())
+)
 tempspec = MinMaxSpec((tmin=Hour(5), tmax=Hour(14)), BSpline(Linear()))
 specs = (temp=tempspec,)
 
-mmseries = minmaxseries(ser, DateTime(2000, 12, 31):Hour(1):DateTime(2001, 12, 31), (), specs)
-iseries = interpseries(ser, DateTime(2019, 1):Hour(1):DateTime(2019, 12, 31), (tmax=BSpline(Linear()), tmin=BSpline(Linear()),))
-
-mmseries[DateTime(2019, 1, 5, 6)][:temp]
-
-
-####################
-
-# WorldClim Weather
-
-# Weather time-series
-ser = series(WorldClim{Climate}; layers=(:tmin,), resolution="10m", months=Jan:March)
-ser[Jan][:tmin]
-# Select Australia, using regular lat/lon selectors
-A = geoarray(WorldClim{Climate}, :tmin, "10m", Feb; mappedcrs=EPSG(4326))
-A[Lat(Between(-10, -45)), Lon(Between(110, 160))]
+mmseries = meandayminmaxseries(ser, dates, (), specs)
+mmseries[DateTime(2001, 1, 1, 6)][:temp]
+mmseries[DateTime(2001, 4, 1, 1)][:temp]
+mmseries[DateTime(2001, 9, 1, 13)][:temp]
+mmseries[DateTime(2001, 11, 1, 2)][:temp]
 
 using Plots
-A[Lat(Between(-10, -45)), Lon(Between(110, 160))] |>
-   plot
+mmseries[DateTime(2001, 11, 1, 2)][:temp] |> plot
 
 
-layers = (:tmin, :tmax)
-ser = series(WorldClim{Climate}; layers = layers, resolution="10m", months=Jan:Dec)
+basedir = @__DIR__
+year = 2016
 
-dates = (Date(2001), Date(2002))
-ser[Jan][:tmax]
 
-# Weather time-series
-index(ser, Ti)
-ser[Jan][:tmin]
-ser[Jan][:tmax]
+using GrowthMaps, Plots, Unitful, UnitfulRecipes, Dates, Setfield, Statistics
+using GeoData, ArchGDAL, NCDatasets, HDF5, ModelParameters, CSV
+using Unitful: °C, K, cal, mol
 
-mmseries = minmaxseries(ser, DateTime(2019, 1):Hour(1):DateTime(2019, 12, 31), (), specs)
+ψ_EA =12.254cal/K/mol
+ΔH_A = Param(11822.57; units=cal/mol, bounds=(1e5, 2e5))
+ΔH_H = Param(90678.50; units=cal/mol, bounds=(0.5e5, 1e5))
+ΔH_L = Param(-6603.414; units=cal/mol, bounds=(0.5e5, 1e5))
+p27 = Param(0.75; bounds=(0.0, 1.0)) # this is higher than 0.17 as
+T_ref = K(27.0°C)
+S_H = 295.08cal/mol/K
+S_L = -19.59cal/mol/K
+Thalf_L = Param(ustrip(K, withunits(ΔH_L)/S_L); units=K, bounds=(290, 350))
+Thalf_H = Param(ustrip(K, withunits(ΔH_H)/S_H); units=K, bounds=(290, 350))
 
-iseries = interpseries(ser, DateTime(2019, 1):Hour(1):DateTime(2019, 12, 31), (tmax=BSpline(Linear()), tmin=BSpline(Linear()),))
-awapstack = stack(AWAP; date=DateTime(2019, 1, 5))
-mmseries[DateTime(2019, 1, 5, 6)][:temp]
+growthmodel = SchoolfieldIntrinsicGrowth(p27, ΔH_A, ΔH_L, Thalf_L, ΔH_H, Thalf_H, T_ref)
+growthresponse = Layer(:surface_temp, K, growthmodel)
+
+GrowthMaps.rate(stripparams(growthresponse), T_ref)
+
+temprangeC = collect(1.0:1.0:40.0)°C
+temprange = K.(temprangeC)
+# GrowthMaps.rate.(Ref(growthresponse), temprange)
+dev = (x -> GrowthMaps.rate(stripparams(growthresponse), x)).(temprange)
+p = plot(temprangeC, dev; label=false, ylabel="growth rate (1/d)", xlab = "temperature")
+
+
+
+
+##### SMAP
+smapfolder = "K:/SMAP/SMAP_L4_SM_gph_v4"
+smapfolder = "K:/SMAP/SMAP_monthly_midpoint_subset"
+# This does the same as the above
+days = (1, )
+seriesall = SMAPseries(smapfolder)
+series = seriesall[Where(t -> (t >= DateTime(year) && t < DateTime(year)) && dayofmonth(t) in days)]
+
+#' We can plot a layer from a file at some date in the series:
+seriesall[2][:surface_temp] |> plot
+
+####
+
+
+
+growthrates = mapgrowth(growthresponse;
+    series=seriesall,
+    tspan=DateTime(2017, 1, 1):Month(1):DateTime(2017, 12, 1)
+)
+plot(growthrates[Ti(1)], clim=(0, 0.15))
